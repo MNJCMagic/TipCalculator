1287a4dff35ae75f1d2d42854ad00b9f93b2ef4a
Mike Cameron <mike@mikecameron.tv>
2018-04-20 11:27:30 -0400

    Initial Commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Mike Cameron on 2018-04-20.
+//  Copyright © 2018 Mike Cameron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Mike Cameron on 2018-04-20.
+//  Copyright © 2018 Mike Cameron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
